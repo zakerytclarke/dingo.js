7cb500e0390cf93928a6d7ccb95589ee5574c224
Zakery Clarke <30702789+zakerytclarke@users.noreply.github.com>
2018-06-20 15:55:50 -0600

    Add files via upload

--- dingo.js
+++ dingo.js
@@ -107,6 +107,12 @@ function elem(id){
   return document.getElementById(id);
 }
 function include(file){
-
-
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+   if (this.readyState==4&&this.status==200){
+     document.write(this.responseText);
+   }
+ };
+ xhttp.open("GET",file,true);
+ xhttp.send();
 }
